9cef409ea460f71da8a5784e6b8e686cabfccbdf
heetaeyang <htyang90@gmail.com>
2020-07-03 15:48:19 -0700

    Simple craigslist crawler with reformatted output

--- web_crawler.py
+++ web_crawler.py
@@ -1,26 +1,52 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 class web_crawler(object):
     def __init__(self, query, max_price):
         self.max_price = max_price
         self.query = query
         self.url = f"https://seattle.craigslist.org/search/sss?query={query}&max_price={max_price}"
-        self.driver = webdriver.Chrome("/Users/heetaeyang/Documents/Project/web_crawler/chromedriver")
+        self.driver = webdriver.Chrome(
+            "/Users/PATH_TO_CHROMEDRIVER")
         self.delay = 5
 
-        def load_page(self):
-            driver = self.driver
-            driver.get(self.url)
+    def load_page(self):
+        driver = self.driver
+        driver.get(self.url)
+        all_data = driver.find_elements_by_class_name("result-row")
+        for data in all_data:
+            title = data.text.split("$")
+            if title[0] == "":
+                title = title[1]
+            else:
+                title = title[0]
+
+            title = data.text.split("\n")
+            price = title[0]
+            title = title[-1]
+
+            title = title.split(" ")
+            month = title[0]
+            day = title[1]
+            date = month + " " + day
+
+            title = " ".join(title[2:])
+            price_idx = title.find("$")
+            f_title = title[:price_idx]
+
+            price_and_loc = title[price_idx:]
+            print("Date:", date)
+            print("Item:", f_title)
+            print("Price and location:", price_and_loc)
+            print()
+
+    def close_web_driver(self):
+        self.driver.close()
+        print("\nGoodbye!")
 
 
-
-url = f"https://seattle.craigslist.org/search/sss?query=monitor&max_price=700"
-query = monitor
-max_price = 700
+query = "monitor"
+max_price = "700"
 crawler = web_crawler(query, max_price)
 crawler.load_page()
+crawler.close_web_driver()
